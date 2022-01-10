586eb9565a2acbcbebcd2cba9c3b0bf05550a09f
eliascoelho911 <elias_coelho911@hotmail.com>
2022-01-10 14:33:43 -0300

    feat: Criação da funcionalidade de criar uma nova sala

---
+++ app/src/main/java/com/github/eliascoelho911/youplay/common/SerializeDataClass.kt
@@ -0,0 +1,19 @@
+package com.github.eliascoelho911.youplay.common
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+fun <T> T.serializeToMap(): Map<String, Any> {
+    return convert()
+}
+
+inline fun <reified T> Map<String, Any>.toDataClass(): T {
+    return convert()
+}
+
+inline fun <I, reified O> I.convert(): O {
+    val gson = Gson()
+    val json = gson.toJson(this)
+
+    return gson.fromJson(json, object : TypeToken<O>() {}.type)
+}
