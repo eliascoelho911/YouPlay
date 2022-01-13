2a7f4c2fd152dc0e0b267c87981f148f14b104a6
eliascoelho911 <elias_coelho911@hotmail.com>
2022-01-13 14:51:21 -0300

    refactor: Implementa argumentos default no `Music`

--- app/src/main/java/com/github/eliascoelho911/youplay/domain/entities/Music.kt
+++ app/src/main/java/com/github/eliascoelho911/youplay/domain/entities/Music.kt
@@ -1,3 +1,9 @@
 package com.github.eliascoelho911.youplay.domain.entities
 
-data class Music(val id: ID, val name: String, val durationInSeconds: Int, val artists: List<Artist>, val album: Album)
+data class Music(
+    val id: ID,
+    val name: String,
+    val durationInSeconds: Int,
+    val artists: List<Artist> = emptyList(),
+    val album: Album,
+)
