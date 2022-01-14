d02c98638362e63a9b9a8425ce4e825d7b6d54be
eliascoelho911 <elias_coelho911@hotmail.com>
2022-01-14 16:34:59 -0300

    test: Fluxo de alteração do nome da sala

---
+++ app/src/test/java/com/github/eliascoelho911/youplay/presentation/screens/roomdetails/RoomDetailsViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.github.eliascoelho911.youplay.presentation.screens.roomdetails
+
+import com.github.eliascoelho911.youplay.BaseTest
+import com.github.eliascoelho911.youplay.domain.entities.Room
+import com.github.eliascoelho911.youplay.domain.usecases.room.ObserveCurrentMusic
+import com.github.eliascoelho911.youplay.domain.usecases.room.ObserveCurrentRoom
+import com.github.eliascoelho911.youplay.domain.usecases.room.UpdateCurrentRoom
+import com.github.eliascoelho911.youplay.domain.usecases.user.UserExitFromRoom
+import com.github.eliascoelho911.youplay.roomMock
+import io.mockk.CapturingSlot
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class RoomDetailsViewModelTest : BaseTest() {
+    @MockK
+    private lateinit var userExitFromRoom: UserExitFromRoom
+
+    @MockK
+    private lateinit var updateCurrentRoom: UpdateCurrentRoom
+
+    @MockK
+    private lateinit var observeCurrentRoom: ObserveCurrentRoom
+
+    @MockK
+    private lateinit var observeCurrentMusic: ObserveCurrentMusic
+
+    @InjectMockKs
+    private lateinit var viewModel: RoomDetailsViewModel
+
+    @Test
+    fun testUpdateCurrentName() {
+        val name = "newName"
+        val currentRoom = roomMock
+
+        coEvery { updateCurrentRoom.update(any()) } returns Unit
+
+        runBlocking {
+            viewModel.updateCurrentRoomName(name)
+        }
+
+        val roomModificationSlot = CapturingSlot<suspend Room.() -> Room>()
+        coVerify { updateCurrentRoom.update(capture(roomModificationSlot)) }
+
+        runBlocking {
+            val expected = roomModificationSlot.captured.invoke(currentRoom)
+            assertEquals(expected, currentRoom.copy(name = name))
+        }
+    }
+}
