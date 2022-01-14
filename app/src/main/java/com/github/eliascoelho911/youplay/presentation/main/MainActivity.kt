57f386aeb61d7129227e839bb4f1888e11dc1a89
eliascoelho911 <elias_coelho911@hotmail.com>
2022-01-14 17:32:32 -0300

    refactor: Exporta screenImpl para arquivos separadas

--- app/src/main/java/com/github/eliascoelho911/youplay/presentation/main/MainActivity.kt
+++ app/src/main/java/com/github/eliascoelho911/youplay/presentation/main/MainActivity.kt
@@ -5,25 +5,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
-import com.github.eliascoelho911.youplay.R
 import com.github.eliascoelho911.youplay.presentation.navigation.Destination
-import com.github.eliascoelho911.youplay.presentation.screens.createroom.CreateRoomScreen
-import com.github.eliascoelho911.youplay.presentation.screens.createroom.CreateRoomViewModel
-import com.github.eliascoelho911.youplay.presentation.screens.roomdetails.RoomDetailsScreen
-import com.github.eliascoelho911.youplay.presentation.screens.roomdetails.RoomDetailsViewModel
+import com.github.eliascoelho911.youplay.presentation.screens.accessroom.accessRoomScreenImpl
+import com.github.eliascoelho911.youplay.presentation.screens.createroom.createRoomScreenImpl
+import com.github.eliascoelho911.youplay.presentation.screens.roomdetails.roomDetailsScreenImpl
 import com.github.eliascoelho911.youplay.presentation.theme.YouPlayTheme
-import com.github.eliascoelho911.youplay.presentation.util.RoomIDGenerator
-import com.github.eliascoelho911.youplay.presentation.util.navigate
 import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -54,78 +42,22 @@ class MainActivity : ComponentActivity() {
                     navController = navController,
                     startDestination = Destination.CreateRoom.baseRoute
                 ) {
-                    createRoomScreen(navController)
-                    roomDetailsScreen(navController)
+                    createRoomScreenImpl(navGraphBuilder = this,
+                        navController,
+                        showError = { showError(it) })
+                    roomDetailsScreenImpl(navGraphBuilder = this,
+                        navController,
+                        showError = { showError(it) })
+                    accessRoomScreenImpl(navGraphBuilder = this,
+                        navController)
                 }
             }
-        }
-    }
-
-    private fun NavGraphBuilder.createRoomScreen(navController: NavHostController) {
-        composable(Destination.CreateRoom.baseRoute) {
-            val viewModel: CreateRoomViewModel by viewModel()
-            var createRoomButtonIsLoading by remember { mutableStateOf(false) }
-
-            CreateRoomScreen(viewModel = viewModel,
-                onClickToCreateRoom = {
-                    createRoomButtonIsLoading = true
-                    val roomId = RoomIDGenerator.generate()
-                    lifecycleScope.launch {
-                        runCatching {
-                            viewModel.createNewRoom(roomId)
-                            viewModel.enterTheRoom(roomId)
-                        }.onSuccess {
-                            navController.navigate(Destination.RoomDetails)
-                        }.onFailure {
-                            showError(getString(R.string.error_create_new_room))
-                            createRoomButtonIsLoading = false
-                        }
-                    }
-                },
-                onClickToEnterRoom = { /*TODO*/ },
-                createRoomButtonIsLoading = createRoomButtonIsLoading)
         }
     }
 
     private fun showError(message: String) {
         //TODO Mostrar o erro de uma forma mais bonita
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun NavGraphBuilder.roomDetailsScreen(navController: NavHostController) {
-        composable(Destination.RoomDetails.baseRoute) {
-            val viewModel: RoomDetailsViewModel by viewModel()
-
-            RoomDetailsScreen(viewModel = viewModel,
-                backgroundColor = Color.Blue,
-                onConfirmExitFromRoom = {
-                    lifecycleScope.launch {
-                        runCatching {
-                            viewModel.userExitFromRoom()
-                        }.onSuccess {
-                            navController.popBackStack()
-                        }.onFailure {
-                            showError(getString(R.string.error_exit_room))
-                        }
-                    }
-                },
-                onClickOptions = {},
-                onUpdateRoomName = { name ->
-                    lifecycleScope.launch {
-                        runCatching {
-                            viewModel.updateCurrentRoomName(name)
-                        }.onFailure {
-                            showError(getString(R.string.errorUpdateRoomName))
-                        }
-                    }
-                },
-                onClickSkipToNextMusicButton = {},
-                onClickPlayOrPauseButton = {},
-                onClickSkipToPreviousMusicButton = {},
-                onClickShuffleButton = {},
-                onClickRepeatButton = {},
-                onTimeChange = {})
-        }
     }
 
     private fun requestUserAuthentication() {
---
+++ app/src/main/java/com/github/eliascoelho911/youplay/presentation/screens/roomdetails/RoomDetailsScreenImpl.kt
@@ -0,0 +1,52 @@
+package com.github.eliascoelho911.youplay.presentation.screens.roomdetails
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import com.github.eliascoelho911.youplay.R
+import com.github.eliascoelho911.youplay.presentation.navigation.Destination
+import com.google.accompanist.navigation.animation.composable
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+fun ComponentActivity.roomDetailsScreenImpl(
+    navGraphBuilder: NavGraphBuilder,
+    navController: NavHostController,
+    showError: (error: String) -> Unit,
+) {
+    navGraphBuilder.composable(Destination.RoomDetails.baseRoute) {
+        val viewModel: RoomDetailsViewModel by viewModel()
+
+        RoomDetailsScreen(viewModel = viewModel,
+            backgroundColor = Color.Blue,
+            onConfirmExitFromRoom = {
+                lifecycleScope.launch {
+                    runCatching {
+                        viewModel.userExitFromRoom()
+                    }.onSuccess {
+                        navController.popBackStack()
+                    }.onFailure {
+                        showError(getString(R.string.error_exit_room))
+                    }
+                }
+            },
+            onClickOptions = {},
+            onUpdateRoomName = { name ->
+                lifecycleScope.launch {
+                    runCatching {
+                        viewModel.updateCurrentRoomName(name)
+                    }.onFailure {
+                        showError(getString(R.string.errorUpdateRoomName))
+                    }
+                }
+            },
+            onClickSkipToNextMusicButton = {},
+            onClickPlayOrPauseButton = {},
+            onClickSkipToPreviousMusicButton = {},
+            onClickShuffleButton = {},
+            onClickRepeatButton = {},
+            onTimeChange = {})
+    }
+}
