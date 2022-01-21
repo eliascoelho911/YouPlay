df9d56c3199cb43e6531343152932df53d9d8683
eliascoelho911 <elias_coelho911@hotmail.com>
2022-01-21 18:35:41 -0300

    test: Acesso a sala utilizando qr code

--- app/src/test/java/com/github/eliascoelho911/youplay/presentation/ui/main/MainViewModelTest.kt
+++ app/src/test/java/com/github/eliascoelho911/youplay/presentation/ui/main/MainViewModelTest.kt
@@ -2,9 +2,11 @@ package com.github.eliascoelho911.youplay.presentation.ui.main
 
 import com.github.eliascoelho911.youplay.BaseTest
 import com.github.eliascoelho911.youplay.domain.usecases.spotify.AuthenticateUserOnSpotify
+import com.github.eliascoelho911.youplay.domain.usecases.user.EnterTheRoom
 import com.github.eliascoelho911.youplay.domain.util.spotify.UserIsAuthenticatedOnSpotify
 import com.github.eliascoelho911.youplay.domain.util.user.UserIsInSomeRoom
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.runBlocking
@@ -21,6 +23,9 @@ class MainViewModelTest : BaseTest() {
     @MockK
     private lateinit var userIsInSomeRoom: UserIsInSomeRoom
 
+    @MockK
+    private lateinit var enterTheRoom: EnterTheRoom
+
     @InjectMockKs
     private lateinit var mainViewModel: MainViewModel
 
@@ -32,5 +37,18 @@ class MainViewModelTest : BaseTest() {
             mainViewModel.userIsInSomeRoom()
             assertTrue(mainViewModel.userIsInSomeRoom())
         }
+    }
+
+    @Test
+    fun testEnterTheRoom() {
+        val id = "id"
+
+        coEvery { enterTheRoom.enter(id) } returns Unit
+
+        runBlocking {
+            mainViewModel.enterTheRoom(id)
+        }
+
+        coVerify { enterTheRoom.enter(id) }
     }
 }
